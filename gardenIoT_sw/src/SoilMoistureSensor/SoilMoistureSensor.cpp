6c19d66cd1b66f1d4380d02a62eede6c6bf91cc9
Patrik <patrikludvik@gmail.com>
2025-05-28 18:45:13 +0200

    delet clasy

--- gardenIoT_sw/src/SoilMoistureSensor/SoilMoistureSensor.cpp
+++
@@ -1,9 +0,0 @@
-#include "SoilMoistureSensor.h"
-
-SoilMoistureSensor::SoilMoistureSensor(int pin) : _pin(pin) {
-    pinMode(_pin, INPUT);
-}
-
-int SoilMoistureSensor::readMoisture() {
-    return analogRead(_pin);
-}
